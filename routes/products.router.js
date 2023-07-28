b20ee375220b3fbcf9740ec8c198c1a177b59ad1
PabloOro27 <pabloorozco2712@gmail.com>
2023-07-27 23:31:09 -0600

    [products]manejo de errores con -boom y validacion de datos con -Joi schema

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,13 +1,23 @@
 // aqui se definen los end point de products
 const express = require('express'); // importamos express
 const router = express.Router(); // creamos una instancia de express
-// servicios de la api
+// -----------------------------------------------
+// para validacion de datos traemos el middleware y el schema
+const validatorHandler = require('../middlewares/validator.handler'); // importamos el middleware de validacion
+const {
+  createProductsSchema,
+  updateProductsSchema,
+  getProductsSchema,
+} = require('../schemas/products.schema'); // importamos el schema de validacion
+// -----------------------------------------------
+// creamos una instancia del servicio de productos
 const ProductService = require('../services/products.service'); // importamos el servicio de productos
-const service = new ProductService(); // creamos una instancia del servicio de productos
-
+// servicios de la api
+const service = new ProductService();
+// --------------------RUTAS---------------------------
 // ruta products-----------------------------------
-router.get('/', (req, res) => {
-  const products = service.find(); // obtenemos los productos del servicio
+router.get('/', async(req, res) => {
+  const products = await service.find(); // obtenemos los productos del servicio
   // respondemos con un json con los productos de la base de datos
   res.status(200).json(products);
 });
@@ -16,20 +26,31 @@ router.get('/filter', (req, res) => {
   res.send('Soy un filter');
 });
 // ruta products con id----------------------------
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const product = service.findOne(id); // obtenemos el producto del servicio
-  if(!product) return res.status(404).json({ message: 'Product not found' });
-  else {
+router.get('/:id',
+  validatorHandler(getProductsSchema, 'params'), // validamos los datos de entrada
+  async(req, res, next) => {
+  // comprobamos si existe el producto
+  try{
+    const { id } = req.params;
+    const product = await service.findOne(id); // obtenemos el producto del servicio
     // respondemos con un json con los productos de la base de datos
     res.status(200).json(product);
+  }catch(error){
+    next(error); // llamamos al middleware de errores con el error
   }
 });
 // metodo post, ingresar productos-----------------
-router.post('/', (req, res) => {
+router.post('/',
+  validatorHandler(createProductsSchema, 'body'), // validamos los datos de entrada
+  async(req, res, next) => {
   const body = req.body;
   // ingresar producto
-  const newProduct = service.create(body.name, body.price, body.image, body.description);
+  const newProduct = await service.create(
+    body.name,
+    body.price,
+    body.image,
+    body.description,
+  );
   // para enviarle un status al cliente se utiliza el metodo status
   res.status(201).json({
     message: 'Product created',
@@ -37,27 +58,39 @@ router.post('/', (req, res) => {
   });
 });
 // metodo patch o put, actualizar productos---------
-router.patch('/:id', (req, res) => {
-  const { id } = req.params; // obtenemos el id de los parametros
-  const body = req.body; // obtenemos el body
-  // actualizamos el producto
-  service.update(id, body);
-  // respondemos con un json con los productos de la base de datos
-  res.status(202).json({
-    message: 'Product updated',
-    data: body,
-    id,
-  });
+router.patch('/:id',
+  validatorHandler(getProductsSchema, 'params'), // validamos los datos de entrada
+  validatorHandler(updateProductsSchema, 'body'), // validamos los datos de entrada
+  async(req, res, next) => {
+  try{
+    const { id } = req.params; // obtenemos el id de los parametros
+    const body = req.body; // obtenemos el body
+    // actualizamos el producto
+    const product = await service.update(id, body);
+    // respondemos con un json con los productos de la base de datos
+    res.status(202).json(product);
+
+  } catch(error){
+    next(error); // llamamos al middleware de errores con el error
+  }
+
 });
 // metodo delete, eliminar productos---------------
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  // eliminamos el producto
-  service.delete(id);
-  res.json({
-    message: 'Product deleted',
-    id,
-  });
+router.delete('/:id',
+  validatorHandler(getProductsSchema, 'params'), // validamos los datos de entrada
+  async(req, res, next) => {
+  try{
+    const { id } = req.params;
+    // eliminamos el producto
+    await service.delete(id);
+    res.json({
+      message: 'Product deleted',
+      id,
+    });
+  } catch(error){
+    next(error); // llamamos al middleware de errores con el error
+  }
+
 });
 
 
--- services/products.service.js
+++ services/products.service.js
@@ -1,20 +1,26 @@
 const {faker} = require('@faker-js/faker');
+const boom = require('@hapi/boom'); // importamos boom para los errores
 
 // clase maestra para el servicio de productos
 class ProductService {
-  constructor(){
+  constructor() {
     this.products = []; // inicializamos el array de productos
     this.generate(); // llamamos al metodo generate
   }
 
   // metodo para generar los productos
-  generate(){
+  generate() {
     const limit = 100; // limite de productos
     for (let i = 0; i < limit; i++) {
       this.products.push({
         id: (i + 1).toString(),
         name: faker.commerce.productName(),
-        price: faker.commerce.price({min: 100, max: 5000, dec: 2, symbol: 'Q'}),
+        price: faker.commerce.price({
+          min: 100,
+          max: 5000,
+          dec: 2,
+          symbol: 'Q',
+        }),
         image: faker.image.imageUrl(),
         description: faker.commerce.productDescription(),
       });
@@ -22,8 +28,8 @@ class ProductService {
   }
 
   // CRUD'S create read update delete
-  // crear
-  create(name, price, image, description){
+  // crear-------------------------------------------
+  async create(name, price, image, description) {
     const newProduct = {
       id: (this.products.length + 1).toString(),
       name,
@@ -34,19 +40,33 @@ class ProductService {
     this.products.push(newProduct);
     return newProduct;
   }
-  // buscar general
-  find(){
-    return this.products;
+  // buscar general----------------------------------
+  async find() {
+    // simulamos una peticion a una base de datos
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 2000);
+    });
   }
-  // buscar solo uno
-  findOne(id){
+  // buscar solo uno---------------------------------
+  async findOne(id) {
+    const index = this.products.findIndex((item) => item.id === id);
     // buscamos el producto con el id
-    return this.products.find(item => item.id === id);
+    if (index === -1) {
+      // throw new Error('id no found');
+      throw boom.notFound('id not found');
+    }
+    // buscamos el producto con el id
+    return this.products.find((item) => item.id === id);
   }
-  // actualizar
-  update(id, changes){
+  // actualizar--------------------------------------
+  update(id, changes) {
     // buscamos el producto con el id
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('id no found');
+    }
     // actualizamos el producto
     this.products[index] = {
       ...this.products[index],
@@ -54,10 +74,13 @@ class ProductService {
     };
     return this.products[index];
   }
-  // eliminar
-  delete(id){
+  // eliminar----------------------------------------
+  async delete(id) {
     // buscamos el producto con el id
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw boom.notFound('id no found');
+    }
     // eliminamos el producto
     this.products.splice(index, 1);
     return { id };
